20f56e3d24554ecbb36a5f59645ed26fa461ac8d
harishraisinghani <harishraisinghani@gmail.com>
2016-08-25 05:24:19 +0000

    Add and then fadeOut lighthouse pics

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,5 +1,7 @@
 $(function () {
 
+  var response;
+
   $.ajax({
     method: 'get',
     url: 'https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=lighthouse&format=json',
@@ -8,8 +10,15 @@ $(function () {
       },
     dataType: 'jsonp',
     jsonpCallback: 'jsonFlickrApi',
-    success: function(someJSON) {
-      console.log(someJSON);
+    success: function (response) {
+      var result = response.photos.photo;
+      var tempLimit = 4;
+      $.each(result,function(key, value) {
+        if(key>tempLimit) return false;
+
+        $('<img class="dynamic">').attr('src','https://farm'+result[key]["farm"]+'.staticflickr.com/'+result[key]["server"]+'/'+result[key]["id"]+'_'+result[key]["secret"]+'.jpg').appendTo('.lighthouse-pics').fadeOut(1500);
+
+      });
     }
   });
 });
