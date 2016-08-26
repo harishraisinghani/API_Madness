f8a2be452c6504d9807ac342c3de9d96e19ef529
harishraisinghani <harishraisinghani@gmail.com>
2016-08-26 01:51:11 +0000

    Refactor JS code and circulate through pics

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,24 +1,44 @@
 $(function () {
 
-  var response;
+  var PHOTO_LIMIT = 10;
+  var API_KEY = 'b0739d83d22e1a2550a877d51c0c0eb1';
+
+  var photoURL = function(photo) {
+    return 'https://farm'+ photo.farm +'.staticflickr.com/'+ photo.server +'/'+photo.id +'_'+ photo.secret+'.jpg';
+  }
+
+
+  var showPhoto = function(index) {
+    $('#photo'+index).fadeIn(2500);
+    if (index < PHOTO_LIMIT) {
+      $('#photo'+index).fadeOut(2500,function(){
+        showPhoto(index+1);
+      });
+    }
+  }
 
   $.ajax({
     method: 'get',
-    url: 'https://api.flickr.com/services/rest/?method=flickr.photos.search&tags=lighthouse&format=json',
+    url: 'https://api.flickr.com/services/rest/',
     data: {
-      api_key:'b0739d83d22e1a2550a877d51c0c0eb1'
+      api_key: API_KEY,
+      tags: 'lighthouse',
+      format: 'json',
+      method: 'flickr.photos.search'
       },
     dataType: 'jsonp',
     jsonpCallback: 'jsonFlickrApi',
     success: function (response) {
       var result = response.photos.photo;
-      var tempLimit = 4;
       $.each(result,function(key, value) {
-        if(key>tempLimit) return false;
-
-        $('<img class="dynamic">').attr('src','https://farm'+result[key]["farm"]+'.staticflickr.com/'+result[key]["server"]+'/'+result[key]["id"]+'_'+result[key]["secret"]+'.jpg').appendTo('.lighthouse-pics').fadeOut(1500);
-
+        if(key>PHOTO_LIMIT) return false;
+        $('<img>')
+          .attr("id",'photo'+ key)
+          .attr('src', photoURL(value))
+          .appendTo('.lighthouse-pics').hide();
       });
+      showPhoto(0);
     }
   });
 });
+
